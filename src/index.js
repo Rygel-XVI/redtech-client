ddb81a7ac6c83c0c4f5a6e2dbae6cdd0899c9745
Rygel-XVI <micha232@umn.edu>
2019-05-10 08:50:45 -0700

    removed unused code

--- src/index.js
+++ src/index.js
@@ -8,12 +8,6 @@ import rootReducer from './reducers/deckReducer';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
-
-// const store = createStore(
-//     rootReducer, compose( applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
-// );
-
 const store = createStore(
   rootReducer,
   compose(
