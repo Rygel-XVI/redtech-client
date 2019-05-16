3d4a1b60a734f9e3078126c77951ccd3ac5d17d2
Rygel-XVI <micha232@umn.edu>
2019-05-15 17:50:25 -0700

    fixed dynamic rendering of cards and renamed jpg of aces and jacks through kings to be numbers to match the image location the cards are given on the backend

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
-// import logo from '../images/JPEG/2H.jpg'
 
 class Card extends Component {
 
   renderCard(){
-    // if (this.props.card !== null) {
-    //   let cardString = this.props.card.suit + this.props.card.number
-    //   return cardString
-    // } else {
-    //   return "I'm a null card!"
-    // }
+
     if (this.props.card !== null) {
-      // debugger;
       let cardImage = this.props.card.image_location
-      return < img src={require(`${cardImage}`)} name={this.props.name} className='card-image' />
+
+      return < img src={require(`../images/JPEG/${cardImage}.jpg`)} name={this.props.name} className='card-image' />
     } else {
       return < img src={require(`../images/JPEG/blue_back.jpg`)} name={this.props.name} className='card-image' />
-      // return  <button onClick={this.props.handleClick} name={this.props.name}>Pick a Card</button>
     }
   }
 
@@ -26,16 +19,10 @@ class Card extends Component {
       {this.renderCard()}
       <br />
       <button onClick={this.props.handleClick} name={this.props.name}> Draw Card </button>
-         <br />
+      <br />
       </div>
     )
   }
-
+  
 }
 export default Card
-
-
-// this works standalone but still working on dynamically rendering it successfully
-// < img src={require(`../images/JPEG/2C.jpg`)} name={this.props.name} />
-
-// other options include requiring them all separately in an array and trying that
